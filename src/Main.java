5005ee3074fc5507f1125126e886869f26cafcd0
Goennjamin <leon.warren78@gmail.com>
2022-05-26 15:03:34 +0200

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,109 @@
+import java.io.*;
+import java.util.ArrayList;
+
+
+public class Main {
+
+    public void START(){
+
+    }
+
+    public void STOP(){
+
+    }
+
+    public void OUT(){
+
+    }
+
+    public void IN(){
+
+    }
+
+    public void LOAD(){
+
+    }
+
+    public void LOADNUM(){
+
+    }
+
+    public void STORE(){
+
+    }
+
+    public void JUMPNEG(){
+
+    }
+
+    public void JUMPPOS(){
+
+    }
+
+    public void JUMPNULL(){
+
+    }
+
+    public void JUMP(){
+
+    }
+
+    public void ADD(){
+
+    }
+
+    public void ADDNUM(){
+
+    }
+
+    public void SUB(){
+
+    }
+
+    public void MUL(){
+
+    }
+
+    public void SUBNUM(){
+
+    }
+
+    public void MULNUM(){
+
+    }
+
+    public void DIVNUM(){
+
+    }
+
+    public void LOADIND(){
+
+    }
+
+    public void STOREIND(){
+
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        File file = new File("/home/leonw/Dokumente/HAL_Programm.txt");
+        BufferedReader br = new BufferedReader(new FileReader(file));
+        String st;
+
+        ArrayList<String> Befehle = new ArrayList<>();
+
+
+        while((st = br.readLine()) != null){
+
+            Befehle.add(st);
+        }
+
+        int r0,r1,r2,r3,r4,r5,r6,r7,r8,r9,r10,r11,r12,r13,r14,r15,Accu,PC = 0;
+
+
+    }
+}
+
+
+
+
